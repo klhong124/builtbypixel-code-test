c29c6b9641af6ea2a8fe3e301128477e89903c7a
Ryan Kwan <klhong124@gmail.com>
2025-07-04 13:49:53 +0100

    add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import '@/styles/globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Task Management",
@@ -25,9 +26,9 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+          <Analytics />
         </body>
       </html>
     </>
   );
 }
-  
